10bb9eb5226b5bf21dd3bf507f558dc7f1dd73cb
Natasa <peic.natasa@gmail.com>
2016-06-17 13:10:38 -0400

    Unit 10.7. assignment.

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,10 +4,34 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardTwo === cardFour || cardOne === cardTwo || cardThree === cardFour) {
  alert ("You found a match!");
   
 } else {
   alert ("Sorry, try again.");
 }
+*/
 
+
+function createBoard () {
+  
+var boardDiv = document.getElementById("game-board");
+  
+var cards = 4;
+
+ for(var i = 0; i < cards; i++){ 
+        var myDiv = document.createElement("div");
+        myDiv.className = "card";
+        boardDiv.appendChild(myDiv);
+   
+    }
+
+}
+
+
+
+
+whichElem = document.getElementById("cards");
+
+whichElem.onclick = createBoard;
